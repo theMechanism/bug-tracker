8d89fa54755e162c9b5dfcda852d9adc7480d8a7
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2014-06-16 17:12:49 -0400

    initial bug fixes

--- app/admin/bug.rb
+++ app/admin/bug.rb
@@ -4,6 +4,7 @@ ActiveAdmin.register Bug do
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
+  
   permit_params [:project_id, :url, :browser, :status, :params, :width, :height, :description, :os, :ua, :browser_version]
   index do
     selectable_column
@@ -14,11 +15,11 @@ ActiveAdmin.register Bug do
       obj.created_at.to_date
     end
     column("Status")   {|bug| status_tag(bug.status)} 
+    
     actions
   end
   form do |f|
       f.inputs "Bug" do
-        f.input :description
         f.input :status, :as=> :select,:include_blank => false, :collection => ['Open', "Closed", "Verify"]
       end
       f.actions
--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,6 +1,12 @@
 class ClientsController < Devise::RegistrationsController
-	
+	 before_filter :authenticate!
 	def show
+		
 		@client = Client.find(params[:id])
+	end
+	def authenticate!
+		if current_client == nil
+			redirect_to new_client_session_path, notice: "Please sign in before continuing"
+		end
 	end
 end
