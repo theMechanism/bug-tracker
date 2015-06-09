53e6d5313c7a90b801eef4608037eaee812227e7
avifoxi <frosenox@gmail.com>
2015-06-09 16:55:16 -0400

    add url validations to project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,12 @@
 class Project < ActiveRecord::Base
-	 validates_presence_of :name
-	 has_many :bugs,
-    	dependent: :destroy
-    belongs_to :client,
- 	  inverse_of: :project
- 	validates :name, uniqueness: { scope: :client }
+  validates_presence_of :name
+	has_many :bugs,
+    dependent: :destroy
+  belongs_to :client
+  
+  validates :name, uniqueness: { scope: :client }, presence: true
+  validates_associated :client
+
+  validates :git_repo_url, format: { with: URI.regexp }, if: Proc.new { |a| a.git_repo_url.present? }, allow_nil: true
+  validates :dev_server_url, format: { with: URI.regexp }, if: Proc.new { |a| a.dev_server_url.present? }, allow_nil: true
 end
