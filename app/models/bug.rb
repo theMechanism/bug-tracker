19fb84d27055fc7c1c6643239ba6cb587b799685
avifoxi <frosenox@gmail.com>
2015-06-10 10:17:34 -0400

    adding bug tests

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -1,9 +1,14 @@
 class Bug < ActiveRecord::Base
+
+  belongs_to :admin
+  belongs_to :project,
+    inverse_of: :bugs
+
 	VALID_STATUS = ['Open', 'Closed', 'Verify']
-	validates_presence_of :description, :name
-	belongs_to :project,
- 	  inverse_of: :bugs
- 	validates_inclusion_of :status,
-    	in: VALID_STATUS
+	
+  validates_presence_of :description, :name
+ 	validates_inclusion_of :status, in: VALID_STATUS
+  validates_associated :project, allow_nil: false
+  validates_associated :admin, allow_nil: true
     	
 end
--- spec/models/bug_spec.rb
+++ spec/models/bug_spec.rb
@@ -1,15 +1,29 @@
 require 'spec_helper'
 
 RSpec.describe Bug, :type => :model do
+
+  let(:bug) { FactoryGirl.create :bug }
+  
   context "has valid factory" do
     it "so buggy" do
-      bug = build(:bug)
       expect(bug).to be_a(Bug)
-    end
+    end 
+  end
+
+  context 'validations' do 
     it { should validate_presence_of :description }  
     it { should belong_to :project } 
     it { should_not have_valid(:status).when('dsgufgdsuifgeg',12325,'', " ") }
     it { should have_valid(:status).when('Open','Closed','Verify') }
- 
+
+    it 'accepts associated admin' do
+      bug.admin = create(:admin)
+      expect(bug.valid?).to be(true)
+    end
+
+    it 'requires associated project' do
+      bug.project = nil
+      expect(bug.valid?).to be(false)
+    end
   end
 end
