4d645d19be3b79114b02ff44683d008cc75ddbe6
avifoxi <frosenox@gmail.com>
2015-06-11 13:51:34 -0400

    project show, skeleton of bug show

--- app/controllers/dashboard/bugs_controller.rb
+++ app/controllers/dashboard/bugs_controller.rb
@@ -2,6 +2,7 @@ module Dashboard
   class BugsController < ApplicationController
     include ApplicationHelper
     before_filter :deny_access, :unless => :is_admin?
+    
     def index
     end
 
@@ -9,10 +10,17 @@ module Dashboard
     end
 
     def show
+        @bug = Bug.find(params[:id])
+        @project = @bug.project
+        @admin = @bug.admin
+        @comments = @bug.comments
     end
+    
     def update
         p params
+
     end
+    
     def destroy
     end
   end
