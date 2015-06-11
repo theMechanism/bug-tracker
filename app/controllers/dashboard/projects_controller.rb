772836b7a71ed45e20a92429e6374294f4f5488e
avifoxi <frosenox@gmail.com>
2015-06-11 14:28:52 -0400

    bug detail partial

--- app/controllers/dashboard/projects_controller.rb
+++ app/controllers/dashboard/projects_controller.rb
@@ -2,6 +2,7 @@ module Dashboard
   class ProjectsController < ApplicationController
     include ApplicationHelper
     before_filter :deny_access, :unless => :is_admin?
+    
     def index
     end
 
@@ -9,15 +10,14 @@ module Dashboard
     end
 
     def show
-        p '#'*80
-        p "#{params[:id]}"
-        p '#'*80
         @project = Project.find(params[:id])
         @bugs = @project.bugs
         @admins = Admin.all
     end
+    
     def update
     end
+    
     def destroy
     end
   end
