4a78500111eb4fc59fc3e4132f4db36db94c0ebb
avifoxi <frosenox@gmail.com>
2015-06-17 12:47:57 -0400

    truckin along, json interface

--- app/controllers/dashboard/clients_controller.rb
+++ app/controllers/dashboard/clients_controller.rb
@@ -8,6 +8,17 @@ module Dashboard
     end
 
     def create
+        @client = Client.new(client_params) 
+        p '#'*80
+        p 'params'
+        p "#{params.inspect}"
+        if @client.save
+            render json: { 
+                redirect_url: dashboard_client_path(@client)
+            }
+        else
+            render :new, layout: false
+        end
     end
 
     def show
@@ -21,5 +32,12 @@ module Dashboard
         @client = Client.new
         render layout: false
     end
+
+    private
+
+    def client_params
+        params.require(:client).permit(:name_of_co, :name_of_primary_contact, :email, :phone, :password, :misc_info)
+    end
+
   end
 end
