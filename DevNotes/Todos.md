c01a6288241e27e5f1aba951dad43b01f0410e97
avifoxi <frosenox@gmail.com>
2015-06-23 13:27:47 -0400

    adding clients controller tests -- all get methods tested

---
+++ DevNotes/Todos.md
@@ -0,0 +1,6 @@
+June 23 ---
+
+destroy actions in controllers (none exist)
+  - dashboard / clients
+
+hunt down JS oddness, with alerts + comment posting
--- spec/controllers/dashboard/clients_controller_spec.rb
+++ spec/controllers/dashboard/clients_controller_spec.rb
@@ -1,22 +1,100 @@
 require 'spec_helper'
 
-RSpec.describe Dashboard::CommentsController, :type => :controller do
+RSpec.describe Dashboard::ClientsController, :type => :controller do
 
-  # routes { Dashboard::Engine.routes }
+  before do |example|
+    unless example.metadata[:skip_login]
+      sign_in create(:admin)
+    end
+    unless example.metadata[:no_create]
+      @client = create(:client)
+    end
+  end
+
+  describe "without current_admin", :skip_login, :no_create do 
+    it 'redirects away from controller actions' do
+      get :index
+      expect(response).to redirect_to(new_admin_session_path)
+    end
+  end
 
   describe "GET index" do
-    it "assigns @teams" do
-      client = create(:client)
-      p 'check self'
-      p '#'*80
-      # p "#{self::Engine.inspect}"
-      get :index, use_route: :dashboard
-      # expect(assigns(:clients)).to eq([client])
+    it "assigns @clients" do   
+      get :index
+      expect(assigns(:clients)).to eq([@client])
     end
 
-    # it "renders the index template" do
-    #   get :index
-    #   expect(response).to render_template("index")
-    # end
+    it "assigns @modal_urls" do 
+      get :index
+      expected_json = {
+        new_client_project: new_dashboard_project_path,
+        new_client: new_dashboard_client_path
+      }.to_json
+      expect(assigns(:modal_urls)).to eq(expected_json )
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+      expect(response).to render_template(layout: 'dashboard')
+    end
   end
+
+  describe "GET new", :no_create do
+    it 'renders form partial without layout' do 
+      get :new
+      expect(response).to render_template('new')
+      expect(response).to render_template(layout: nil)
+    end
+    it 'assigns new @client' do 
+      get :new
+      expect(assigns(:client)).to be_a(Client)
+    end
+  end
+
+  describe "GET edit" do
+    it 'renders form partial without layout' do
+      get :edit, id: @client.id
+      expect(response).to render_template('edit')
+      expect(response).to render_template(layout: nil)
+    end
+    it 'assigns @client' do 
+      get :edit, id: @client.id
+      expect(assigns(:client)).to eq(@client)
+    end
+  end
+
+  describe "GET show" do 
+    it 'renders template' do
+      get :show, id: @client.id
+      expect(response).to render_template('show')
+      expect(response).to render_template(layout: 'dashboard')
+    end
+    it 'assigns @client' do 
+      get :show, id: @client.id
+      expect(assigns(:client)).to eq(@client)
+    end
+    it "assigns @modal_urls" do 
+      get :show, id: @client.id
+      expected_json = {
+        new_client_project: new_dashboard_project_path,
+        edit_client: edit_dashboard_client_path(@client)
+      }.to_json
+      expect(assigns(:modal_urls)).to eq(expected_json )
+    end
+  end
+
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
