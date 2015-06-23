0ddc7c65af7629e4ae470125057447e020cb80d4
avifoxi <frosenox@gmail.com>
2015-06-23 15:25:44 -0400

    add skeleton of tests in bugs ctrl
    ;

--- spec/controllers/dashboard/bugs_controller_spec.rb
+++ spec/controllers/dashboard/bugs_controller_spec.rb
@@ -2,4 +2,28 @@ require 'spec_helper'
 
 RSpec.describe Dashboard::BugsController, :type => :controller do
 
+  describe 'GET new' do 
+    it '' do 
+
+    end
+  end
+
+  describe 'GET show' do 
+    it '' do 
+
+    end
+  end
+
+  describe 'POST create' do 
+    it '' do 
+
+    end
+  end
+
+  describe 'PUT update' do 
+    it '' do 
+
+    end
+  end
+
 end
