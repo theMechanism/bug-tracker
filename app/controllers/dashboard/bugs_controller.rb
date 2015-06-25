50f458248d1817594851acda91e605cd46f31244
avifoxi <frosenox@gmail.com>
2015-06-25 13:45:28 -0400

    add edit bug to bug show

--- app/controllers/dashboard/bugs_controller.rb
+++ app/controllers/dashboard/bugs_controller.rb
@@ -14,6 +14,11 @@ module Dashboard
         render layout: false
     end
 
+    def edit
+        @bug = Bug.find(params[:id])
+        render layout: false
+    end
+
     def create
         @project = Project.find(params[:project_id])
         @bug = @project.bugs.build(bug_params)
@@ -32,6 +37,9 @@ module Dashboard
         @admin = @bug.admin
         @comments = @bug.comments.order(:created_at)
         @comment = @bug.comments.new
+        @modal_urls = {
+            edit_bug: edit_dashboard_bug_path(@bug)
+        }.to_json.html_safe
     end
     
     def update 
@@ -40,15 +48,20 @@ module Dashboard
           if @bug.update_attributes(bug_params) #
             return_obj = {}
             return_obj['bug'] = @bug
-            case bug_params.keys[0]
-            when 'admin_id'
-                @admins = Admin.all
-                return_obj['callback'] = 'projectShow.updateTeamLeaderboard'
-                return_obj['html'] = render_to_string(partial: '/dashboard/admins/leaderboard.html.erb', :formats => [:html], locals: {admins: @admins})
-            when 'status'
-                return_obj['callback'] = 'bugTable.updateStatus'
-                return_obj['html'] = render_to_string(partial: '/dashboard/bugs/status_table_cell.html.erb', :formats => [:html], locals: {bug: @bug})
+            if bug_params.keys.count > 1 
+                return_obj['redirect_url'] = dashboard_bug_path(@bug)
+            else
+                case bug_params.keys[0]
+                when 'admin_id'
+                    @admins = Admin.all
+                    return_obj['callback'] = 'projectShow.updateTeamLeaderboard'
+                    return_obj['html'] = render_to_string(partial: '/dashboard/admins/leaderboard.html.erb', :formats => [:html], locals: {admins: @admins})
+                when 'status'
+                    return_obj['callback'] = 'bugTable.updateStatus'
+                    return_obj['html'] = render_to_string(partial: '/dashboard/bugs/status_table_cell.html.erb', :formats => [:html], locals: {bug: @bug})
+                end
             end
+            
                  
             format.json { render json: return_obj }
           else
