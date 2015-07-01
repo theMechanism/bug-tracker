24822c76b0e76d641cd18f219eacb8cc47f99a7a
avifoxi <frosenox@gmail.com>
2015-07-01 16:55:52 -0400

    first callback firing email in bugmailer module

--- app/models/concerns/bug_mailer_alert.rb
+++ app/models/concerns/bug_mailer_alert.rb
@@ -2,18 +2,21 @@ module BugMailerAlert
   extend ActiveSupport::Concern
 
   included do
-    around_save :alert_admin_if_assigned_to_bug
+    after_save :alert_admin_if_assigned_to_bug
   end
 
   def alert_admin_if_assigned_to_bug
+    p '#'*80
+    p 'in callback'
+    p "#{self.inspect}"
     admin_changed = self.admin_id_changed?
     
     if admin_changed
       old_ad_id = self.admin_id_was
       new_ad_id = self.admin_id
       
-      BugMailer.alert_admin_assigned_to_bug(bug, admin_id)
-      BugMailer.alert_admin_unassigned_from_bug(bug, admin_id)
+      BugMailer.alert_admin_assigned_to_bug(self, admin_id).deliver
+      BugMailer.alert_admin_unassigned_from_bug(self, admin_id).deliver
     end
   end
 end
