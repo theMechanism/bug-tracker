dbbaf5a2e9cdbdb4fb798374ca77dd6b8ad6b8b6
avifoxi <frosenox@gmail.com>
2015-07-02 17:31:56 -0400

    last comment -- BYBYE

--- app/controllers/dashboard/widgets_controller.rb
+++ app/controllers/dashboard/widgets_controller.rb
@@ -4,6 +4,7 @@ module Dashboard
     before_filter :deny_access, :unless => :is_admin?
 
     def embeddable_script
+      # CHECK THAT PROJECT IS ACTIVE
       @project = Project.find(params[:project_id])
       render layout: false
     end
