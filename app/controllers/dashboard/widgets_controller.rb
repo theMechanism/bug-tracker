24369495ef4eb34f45da3a769e8ecfff650f8d00
avifoxi <frosenox@gmail.com>
2015-07-02 17:01:01 -0400

    generate widget controller

---
+++ app/controllers/dashboard/widgets_controller.rb
@@ -0,0 +1,2 @@
+class Dashboard::WidgetsController < ApplicationController
+end
---
+++ spec/controllers/dashboard/widgets_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Dashboard::WidgetsController, :type => :controller do
+
+end
