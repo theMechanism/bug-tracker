ef46c5c9b67203e33c70436a8072e6b24faad799
avifoxi <frosenox@gmail.com>
2015-07-06 13:13:49 -0400

    test redirect to login

--- spec/features/iframe_usage_on_test_page_spec.rb
+++ spec/features/iframe_usage_on_test_page_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
 
 describe "iframe actions on test_page", type: :feature do
-  before(:each) do
-    # sign_in create(:admin)
+  before(:each) do |example|
+
     create(:project)
+
+    if example.metadata[:user_clicks_n_expands] 
+      visit dashboard_test_page_path
+      
+      within_frame find('#mech-bug-iframe') do
+        find("#mech-pull-tab div").click
+      end
+    end
   end
 
   describe "renders", :js => true do 
@@ -16,12 +24,15 @@ describe "iframe actions on test_page", type: :feature do
 
     end
   end
-  describe "behavior", :js => true do 
-    it "user can click to expand pull tab, submit bug" do 
-      visit dashboard_test_page_path
-      
+  describe "behavior", :user_clicks_n_expands, :js => true do 
+    it "user can click to expand pull tab, submit bug" do       
       within_frame find('#mech-bug-iframe') do
-        find("#mech-pull-tab div").click
+        expect(page).to have_content('Submit')
+      end
+    end
+
+    it "user can submit legit bug" do 
+      within_frame find('#mech-bug-iframe') do
         find('#mech-bug-report')
         fill_in('Your Name', :with => 'Fake Client')
         fill_in('Description of Bug', :with => 'oh my god this is teeeeerrrrriiiibbbblllleee')
@@ -31,8 +42,26 @@ describe "iframe actions on test_page", type: :feature do
       end
     end
 
-    it "user can submit legit bug" do 
+    it "prevents submission with incomplete user input" do
+      within_frame find('#mech-bug-iframe') do
+        find('#mech-bug-report')
+        # don't fill in fields
+        find('#mech-bug-submit').click
+        find('.mech-bug-message')
+        expect(page).to have_content('error')
+      end
+    end
 
+    it "user clicks 'View Bugs' and opens tab to log in" do 
+      within_frame find('#mech-bug-iframe') do
+        @login_window = window_opened_by do
+          find('#mech-bug-list').click
+        end
+      end
+
+      within_window @login_window do
+        expect(find('.sign-in-submit').value).to eq('Sign in')
+      end
     end
   end
 
