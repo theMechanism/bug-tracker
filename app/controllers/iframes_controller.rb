231a560c0acd7886b6ae2a326e8dc4dfc37218ba
avifoxi <frosenox@gmail.com>
2015-07-08 16:38:35 -0400

    links tab is up

--- app/controllers/iframes_controller.rb
+++ app/controllers/iframes_controller.rb
@@ -8,11 +8,12 @@ class IframesController < ApplicationController
 
   def iframe
     @project = Project.find(params[:id])
+    @bugs = @project.bugs
   end
 
   def project_load_script
     @project = Project.find(params[:id])
-    js = @project.active ? "form.js.erb" : "period_expired.js.erb"
+    js = @project.active ? "load_script.js.erb" : "period_expired.js.erb"
     script = render_to_string(js) 
 
     json = {"js" => script}.to_json
@@ -20,6 +21,7 @@ class IframesController < ApplicationController
     jsonp = callback + "(" + json + ")"
     render :text => jsonp,  :content_type => "text/javascript"
   end
+
   def style
     css = File.read("public/mech-bug-tracker.css").to_s
     json = {"css" => css}.to_json
