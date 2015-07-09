48280795220bdd25426f30aa5cbcdab3315554fe
avifoxi <frosenox@gmail.com>
2015-07-09 11:28:57 -0400

    remove commented code from bugs ctrl, remove overwritign Object prototype in load script.js

--- app/controllers/bugs_controller.rb
+++ app/controllers/bugs_controller.rb
@@ -23,23 +23,7 @@ class BugsController < ApplicationController
 			render json: @bug.errors.full_messages
 		end
 	end
-	def form
-		@project = Project.find(params[:id])
-		js = @project.active ? "form.js.erb" : "period_expired.js.erb"
-		script = render_to_string(js) 
-
-		json = {"js" => script}.to_json
-		callback = params[:callback]
-		jsonp = callback + "(" + json + ")"
-		render :text => jsonp,  :content_type => "text/javascript"
-	end
-	def style
-		css = File.read("public/mech-bug-tracker.css").to_s
-		json = {"css" => css}.to_json
-		callback = params[:callback]
-		jsonp = callback + "(" + json + ")"
-		render :text => jsonp,  :content_type => "text/javascript"
-	end
+	
 	private
 		def bug_params
 				params.require(:bug).permit(:project_id, :name, :url, :browser, :status,
