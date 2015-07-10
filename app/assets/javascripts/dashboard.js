348bb8e88bf0ee6f4f50af22aa5cafd6732f4306
avifoxi <frosenox@gmail.com>
2015-07-10 10:39:46 -0400

    add comment with rationale for application_helper method to retrieve namespaced assets, add dashboard.js manifest

---
+++ app/assets/javascripts/dashboard.js
@@ -0,0 +1,15 @@
+//= require jquery
+//= require jquery_ujs
+//= require twitter/bootstrap
+//= require turbolinks
+//= require react
+//= require react_ujs
+//= require components
+//= require bootstrap
+//= require jquery-tablesorter
+//= require underscore
+//= require_tree ./utils
+//= require_tree ./dashboard
+//= require userAjaxFeedback
+
+console.log('in the dashboard js manifest');
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,25 @@ module ApplicationHelper
     def deny_access
         redirect_to '/admin'
     end
+
+    def namespaced_assets(params)
+      # in the assets pipeline, three manifests are defined
+      # for the three app contexts
+      # 1. dashboard (mech facing for managing projects)
+      # 2. iframe (as lightweight as possible)
+      # 3. client facing app 
+
+      # rails easily retrieves controller specific js 
+      # but needs a custom method (it seems... perhaps I'm wrong)
+      # to retrieve namespaced js, that is not single controller specific
+      # so -- string parse method to retrieve the correct manifest 
+      # this method is not called in iframe layout, bc theres only one controller, and namespace not necessary
+      # but for client + dashboard namespaces, must parse within the js include tag
+      p '$' *80
+      p "in a helper method - whoop"
+      p "#{params.inspect}"
+
+      p "#{params.split('/')[0].inspect}"
+      params.split('/')[0]
+    end
 end
