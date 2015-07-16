25b0c8dc750d61f5369dd1d15debe3d59436353b
avifoxi <frosenox@gmail.com>
2015-07-16 10:18:04 -0400

    move addl function inside local defs, for load script

--- DevNotes/easyXDM___WTF.md
+++ DevNotes/easyXDM___WTF.md
@@ -1,1 +1,7 @@
+the blog post that actually broke this down for me:
+  - http://www.railsdetectives.com/blogs/using-easyxdm-in-rails-to-solve-same-origin-policy-cors-problem
+
+
+
 http://pristinecrap.com/2013/12/31/easyxdm-made-easy-really/
+
--- public/iframe-setup.js
+++ public/iframe-setup.js
@@ -1,13 +1,24 @@
+// the rpc functions are all defined in 
+// views/iframes/load_script.js.erb
+// the three empty objects are stubs until 
+
 var rpc = new easyXDM.Rpc({},
 {
   remote: {
     resizeiFrame: {},
     parentInfo: {},
-    getMyContent: {}
+    customIframeContent: {}
   }
 });
 
 $(document).ready(function(){
   console.log(rpc);
-  rpc.parentInfo(function(parentInfo){console.log(parentInfo.custom_iframe_path)})
+  rpc.parentInfo(function(parentInfo){console.log('via parentInfo func => ' + parentInfo.custom_iframe_path)})
+
+  rpc.customIframeContent(function(customIframeContent){
+    console.log('via customIframeContent => ' + customIframeContent.url);
+  });
+
+
+
 });
