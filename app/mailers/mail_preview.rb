8996e59c79cce9d370fdf01f43f94c9018219145
avifoxi <frosenox@gmail.com>
2015-07-20 13:25:26 -0400

    wrap mail preview in rails.env case

--- app/mailers/mail_preview.rb
+++ app/mailers/mail_preview.rb
@@ -1,30 +1,33 @@
- class MailPreview < MailView
-    
-  def alert_admin_assigned_to_bug
-    @bug = Bug.first
-    @admin_id = Admin.first.id
-    BugMailer.alert_admin_assigned_to_bug(@bug, @admin_id) 
-  end
+case Rails.env
+when 'development'
+  class MailPreview < MailView
+      
+    def alert_admin_assigned_to_bug
+      @bug = Bug.first
+      @admin_id = Admin.first.id
+      BugMailer.alert_admin_assigned_to_bug(@bug, @admin_id) 
+    end
 
-  def alert_admin_unassigned_from_bug
-    bug = Bug.first
-    admin_id = Admin.last.id
-    BugMailer.alert_admin_unassigned_from_bug(bug, admin_id)
-  end
+    def alert_admin_unassigned_from_bug
+      bug = Bug.first
+      admin_id = Admin.last.id
+      BugMailer.alert_admin_unassigned_from_bug(bug, admin_id)
+    end
 
-  def alert_project_manager_that_bug_needs_verification
-    @bug = Bug.last
-    BugMailer.alert_project_manager_that_bug_needs_verification(@bug)
-  end
+    def alert_project_manager_that_bug_needs_verification
+      @bug = Bug.last
+      BugMailer.alert_project_manager_that_bug_needs_verification(@bug)
+    end
 
-  def alert_admin_revert_to_open
-    @bug = Bug.first
-    BugMailer.alert_admin_revert_to_open(@bug)
-  end
+    def alert_admin_revert_to_open
+      @bug = Bug.first
+      BugMailer.alert_admin_revert_to_open(@bug)
+    end
 
-  def alert_admin_is_closed
-    @bug = Bug.first
-    
-    BugMailer.alert_admin_is_closed(@bug)
+    def alert_admin_is_closed
+      @bug = Bug.first
+      
+      BugMailer.alert_admin_is_closed(@bug)
+    end
   end
 end
