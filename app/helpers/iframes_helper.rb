d20ab3ee16c6eb7bed88f70fc87c86cabdfd0f6a
avifoxi <frosenox@gmail.com>
2015-07-20 17:02:22 -0400

    https... not easy!

--- app/helpers/iframes_helper.rb
+++ app/helpers/iframes_helper.rb
@@ -1,6 +1,15 @@
-module IframesHelper
+require 'socket'
 
-  # def javascript_include_tag
-  #   puts 'js baby'
-  # end
+module IframesHelper
+  include Rails.application.routes.url_helpers
+
+  
+  def load_script_via_ssl_connection
+    default_url_options = {
+      host: Rails.env == 'production' ? Socket.gethostname : 'localhost',
+      protocol: 'https'
+    }
+    project_iframe_load_script_url(Project.first, default_url_options)
+  end
+
 end
